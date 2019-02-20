1a4b64541c9fcb128688ae67962b82d4b02b3a71
Paolo Patierno <ppatierno@live.com>
2019-02-20 12:58:13 +0100

    Added skeleton Route for REST API Added configuration class

--- console-server/src/main/java/io/strimzi/ConsoleServer.java
+++ console-server/src/main/java/io/strimzi/ConsoleServer.java
@@ -10,6 +10,8 @@ import org.apache.logging.log4j.Logger;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
+import io.vertx.core.http.HttpServer;
+import io.vertx.ext.web.Router;
 
 public class ConsoleServer extends AbstractVerticle {
 
@@ -18,7 +20,32 @@ public class ConsoleServer extends AbstractVerticle {
     @Override
     public void start(Future<Void> startFuture) throws Exception {
         log.info("Starting ConsoleServer");
-        super.start(startFuture);
+
+        HttpServer server = this.vertx.createHttpServer();
+
+        Router router = Router.router(this.vertx);
+
+        router.get("/topics").handler(routingContext -> {
+            routingContext.response().end("topics");
+        });
+        router.get("/topics/:topicname").handler(routingContext -> {
+            String topicName = routingContext.request().getParam("topicname");
+            routingContext.response().end(topicName);
+        });
+        router.post("/topics/:topicname").handler(routingContext -> {
+            String topicName = routingContext.request().getParam("topicname");
+            routingContext.response().end(topicName);
+        });
+
+        server.requestHandler(router).listen(8080, ar -> {
+            if (ar.succeeded()) {
+                log.info("HTTP REST API server started, listening on port {}", 8080);
+                startFuture.complete();
+            } else {
+                log.error("HTTP REST API failed to start", ar.cause());
+                startFuture.fail(ar.cause());
+            }
+        });
     }
 
     @Override
---
+++ console-server/src/main/java/io/strimzi/ConsoleServerConfig.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2017-2018, Strimzi authors.
+ * License: Apache License 2.0 (see the file LICENSE or http://apache.org/licenses/LICENSE-2.0.html).
+ */
+
+package io.strimzi;
+
+import java.util.Map;
+
+/**
+ * Console server configuration
+ */
+ public class ConsoleServerConfig {
+
+    public static final String STRIMZI_KAFKA_BOOTSTRAP_SERVERS = "STRIMZI_KAFKA_BOOTSTRAP_SERVERS";
+
+    public static final String DEFAULT_KAFKA_BOOTSTRAP_SERVERS = "localhost:9092";
+
+    private final String kafkaBootstrapServers;
+
+    /**
+     * Constructor
+     * 
+     * @param kafkaBootstrapServers the the Kafka bootstrap servers list
+     */
+    public ConsoleServerConfig(String kafkaBootstrapServers) {
+        this.kafkaBootstrapServers = kafkaBootstrapServers;
+    }
+
+    /**
+     * @return the the Kafka bootstrap servers list
+     */
+    public String getKafkaBootstrapServers() {
+        return this.kafkaBootstrapServers;
+    }
+
+    public static ConsoleServerConfig fromMap(Map<String, String> map) {
+        
+        String kafkaBootstrapServers = DEFAULT_KAFKA_BOOTSTRAP_SERVERS;
+        String kafkaBootstrapServersEnvVar = map.get(ConsoleServerConfig.STRIMZI_KAFKA_BOOTSTRAP_SERVERS);
+        if (kafkaBootstrapServersEnvVar != null && !kafkaBootstrapServers.isEmpty()) {
+            kafkaBootstrapServers = kafkaBootstrapServersEnvVar;
+        }
+
+        return new ConsoleServerConfig(kafkaBootstrapServers);
+    }
+
+    @Override
+    public String toString() {
+        return "ConsoleServerConfig(" +
+                "kafkaBootstrapServers=" + kafkaBootstrapServers +
+                ")";
+    }
+ }
