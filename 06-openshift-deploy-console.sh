575cdab88d93ef9eb8e24ae774b5ea28df9ee90b
Ernest Allen <eallen@redhat.com>
2019-03-05 12:53:36 -0500

    Exposed http port 8888

--- 06-openshift-deploy-console.sh
+++ 06-openshift-deploy-console.sh
@@ -3,7 +3,7 @@
 NAMESPACE=${STRIMZI_NAMESPACE:-strimzi}
 CLUSTER=${STRIMZI_CLUSTER:-my-cluster}
 
-sed -i "s/namespace: .*/namespace: $NAMESPACE/" console/install/*RoleBinding*.yaml
-sed -i "s/my-cluster/$CLUSTER/" console/install/050-Deployment-strimzi-console-server.yaml
+sed -i "s/my-cluster/$CLUSTER/" console/install/020-Deployment-strimzi-console.yaml
 
 oc apply -f console/install -n $NAMESPACE
+oc expose service/strimzi-console -n $NAMESPACE
